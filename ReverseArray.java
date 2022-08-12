d974425560934989ba73b3a107267f5b1ae09895
Shreyas Kulkarni <83280989+ShreyasKulkarni96@users.noreply.github.com>
2022-08-12 16:58:07 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,23 @@
+// REVERSE AN ARRAY
+
+public class ReverseArray{
+	
+	public static void main(String [] args){
+		int []  arr = {10,20,30,40,50};
+
+		int low = 0;
+		int high = arr.length-1;
+
+		while(low < high){
+			int temp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = temp;
+
+			low++;
+			high--;
+		}
+		for(int i = 0; i<arr.length; i++){
+		System.out.print(arr[i]);
+		}
+	}
+}
